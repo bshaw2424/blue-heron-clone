29b065f124f8b50286fb2c906ec83c487533bfd0
Bishop Shaw III <prototype124@yahoo.com>
2022-04-14 21:10:30 -0700

    component update

--- src/Navigation.js
+++ src/Navigation.js
@@ -112,7 +112,7 @@ const Navigation = () => {
             </svg>
           </li>
         </ul>
-        <span className="hamburger-nav">
+        <span className="hamburger-nav d-xl-none">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
